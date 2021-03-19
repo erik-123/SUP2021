66e425c20fd5cbd4c52d01d43fc37103bdcc9a82
Axel <axelrosen27@gmail.com>
2021-03-19 10:10:26 +0100

    k

--- SUP2021/SUP2021/ViewModels/profileViewModel.cs
+++ SUP2021/SUP2021/ViewModels/profileViewModel.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using SUP2021.Views;
+
 namespace SUP2021.ViewModels
 {
-    public class profileViewModel
+    public class profileViewModel : BaseViewModel
     {
+        public Command entryCommand { get; }
+
         public profileViewModel()
         {
+            Title = "Profile";
+            entryCommand = new Command(OnentryClicked);
+
         }
+        private async void OnentryClicked(object obj)
+        {
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+        }
+
     }
 }

76b48979a1807c73e317428a6029a054758afd44
axelrosen <Axelrosen27@gmail.com>
2021-04-23 14:17:51 +0200

    * FAQ.xaml: * FAQ.xaml.cs: * FaqViewModel.cs:
    * PaymentTest.xaml: Centrera titel

---
+++ SUP2021/SUP2021/ViewModels/FaqViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace SUP2021.ViewModels
+{
+    public class FaqViewModel : BaseViewModel
+    {
+        public FaqViewModel()
+        {
+
+           
+            
+           Title = "FAQ";
+                
+
+            
+        }
+    }
+}

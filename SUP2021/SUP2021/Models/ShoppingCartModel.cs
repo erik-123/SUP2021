d5bfea5092832885070b3fe94655ffd94b0bd8c2
erik-123 <56879121+erik-123@users.noreply.github.com>
2021-04-23 13:03:52 +0200

    ShoppingCartModel

---
+++ SUP2021/SUP2021/Models/ShoppingCartModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+namespace SUP2021.Models
+{
+    public class ShoppingCartModel
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public Guid ShoppingId { get; set; }
+        [ForeignKey(typeof(User))]
+        [OneToOne]
+        public List<User> user { get; set; }//user
+
+        public int UID { get; set; }//UID
+
+
+
+
+        [ForeignKey(typeof(Products))]
+        public Guid ProductID { get; set; }
+        public ShoppingCartModel() { }
+    }
+
+}
